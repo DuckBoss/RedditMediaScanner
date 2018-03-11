8b2fb57cfe5bb1f0adf516257b30432a897f2b41
Jason <DuckBoss@users.noreply.github.com>
2018-03-11 19:28:51 -0400

    Update and rename RedditImageDownloader.py to ScannyMcScanFace.py

---
+++ ScannyMcScanFace.py
@@ -0,0 +1,89 @@
+import praw
+import wget
+import os
+import datetime as dt
+from time import sleep
+import sys
+
+class ScannyMcScanFace:
+
+    reddit = praw.Reddit("bot1")
+    
+    #Chosen subreddit
+    subreddit_name = "aww"
+    
+    #Allowed File Extensions
+    file_ext = [".jpg", ".png", ".gif"]
+    
+    #Allowed URL Types
+    file_type = ["imgur", "redd.it"]
+    
+    #Chosen search keywords
+    file_seek = [""]
+    
+    #Chosen output directory.
+    output_directory = "./Pics/"
+
+    def __init__(self):
+        print("ScannyMcScanFace Initialized!")
+        print(dt.datetime.now())
+
+    def scan(self):
+        sub = self.reddit.subreddit(self.subreddit_name)
+
+        if not os.path.exists(self.output_directory):
+            os.makedirs(self.output_directory)
+
+        print("Current user: %s" % self.reddit.user.me())
+        self.submissions = list(sub.new(limit=1024))
+        for submission in self.submissions:
+            if any(x in str.upper(submission.title) for x in self.file_seek):
+                submission_url = submission.url
+                if any(y in submission_url for y in self.file_type):
+                    if any(z in submission_url for z in self.file_ext):
+                        print(submission_url)
+                        file_name = submission_url.rsplit('/', 1)[1]
+                        file_dir = ("%s%s" % (self.output_directory, file_name))
+                        if os.path.isfile(file_dir):
+                            continue
+                        else:
+                            wget.download(submission_url, out=self.output_directory)
+
+        print("Historical Pictures Saved!")
+        print("Real time Submissions Capture Active...")
+
+        for submission in sub.stream.submissions():
+            if any(x in str.upper(submission.title) for x in self.file_seek):
+                submission_url = submission.url
+                if any(y in submission_url for y in self.file_type):
+                    if any(z in submission_url for z in self.file_ext):
+                        print(submission_url)
+                        file_name = submission_url.rsplit('/', 1)[1]
+                        file_dir = ("%s%s" % (self.output_directory, file_name))
+                        if os.path.isfile(file_dir):
+                            print("Already Exists - %s" %file_dir)
+                            print("Skipping...")
+                            
+                            print("Waiting for new file...")
+                            sleep(2)
+                            continue
+                        else:
+                            print("Downloaded to - %s" %file_dir)
+                            wget.download(submission_url, out=self.output_directory)
+                            print("Waiting for new file...")
+                            sleep(2)
+                        
+            
+
+
+def main():
+    startTime = dt.datetime.now()
+    program = ScannyMcScanFace()
+    program.scan()
+    endTime = dt.datetime.now()
+    print("Runtime: {}".format(endTime-startTime))
+
+
+
+if __name__ == "__main__":
+    main()
