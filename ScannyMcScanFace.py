afe2cde027ce65f70ae0225e8d266c649e65388c
Jason <DuckBoss@users.noreply.github.com>
2018-03-12 04:24:56 -0400

    Added Support For Gfycat Gifs!
    - Added Gfycat Gif Support
    - Added NSFW Filter (skips any submissions that are 18+)
    - Changed output directory to correspond to chosen subreddit.
    New Dependencies:
    - Urllib
    - Json

--- ScannyMcScanFace.py
+++ ScannyMcScanFace.py
@@ -1,5 +1,7 @@
 import praw
 import wget
+import urllib.request
+import json
 import os
 import datetime as dt
 from time import sleep
@@ -11,18 +13,14 @@ class ScannyMcScanFace:
     
     #Chosen subreddit. Please be mindful of any blacklisted subreddits.
     subreddit_name = "pcmasterrace"
-    
     #Allowed list of File Extensions
     file_ext = [".jpg", ".png", ".gif"]
-    
     #Allowed list of URL Types
-    file_type = ["imgur", "redd.it"]
-    
+    file_type = ["gfycat", "imgur", "redd.it"]
     #Chosen list of search keywords. Please put all keywords in CAPITAL letters.
     file_seek = ["PC"]
-    
-    #Chosen output directory.
-    output_directory = "./DownloadedMedia/"
+    #The output directory is named after the chosen subreddit.
+    output_directory = "./%s/" % subreddit_name
 
     def __init__(self):
         print("ScannyMcScanFace Initialized!")
@@ -30,18 +28,26 @@ class ScannyMcScanFace:
 
     def scan(self):
         sub = self.reddit.subreddit(self.subreddit_name)
-        
-        # Creates output directory if it doesn't exist.
+
         if not os.path.exists(self.output_directory):
             os.makedirs(self.output_directory)
-        
-        # Scans the last 1024 posts to get previously submitted data.
+
         print("Current user: %s" % self.reddit.user.me())
         self.submissions = list(sub.new(limit=1024))
         for submission in self.submissions:
             if any(x in str.upper(submission.title) for x in self.file_seek):
+                if submission.over_18:
+                    continue
                 submission_url = submission.url
                 if any(y in submission_url for y in self.file_type):
+                    if "gfycat" in submission_url:
+                            subSplit = submission_url.rsplit('/', 1)[1]
+                            jsonURL = "http://gfycat.com/cajax/get/%s" % subSplit
+                            with urllib.request.urlopen(jsonURL) as url:
+                                gfyData = json.loads(url.read().decode())
+                                gifURL = gfyData.get('gfyItem').get('gifUrl')
+                                submission_url = gifURL
+
                     if any(z in submission_url for z in self.file_ext):
                         print(submission_url)
                         file_name = submission_url.rsplit('/', 1)[1]
@@ -53,12 +59,21 @@ class ScannyMcScanFace:
 
         print("Historical Pictures Saved!")
         print("Real time Submissions Capture Active...")
-        
-        # Enables realtime submission data capture.
+
         for submission in sub.stream.submissions():
             if any(x in str.upper(submission.title) for x in self.file_seek):
+                if submission.over_18:
+                    continue
                 submission_url = submission.url
                 if any(y in submission_url for y in self.file_type):
+                    if "gfycat" in submission_url:
+                            subSplit = submission_url.rsplit('/', 1)[1]
+                            jsonURL = "http://gfycat.com/cajax/get/%s" % subSplit
+                            with urllib.request.urlopen(jsonURL) as url:
+                                gfyData = json.loads(url.read().decode())
+                                gifURL = gfyData.get('gfyItem').get('gifUrl')
+                                submission_url = gifURL
+
                     if any(z in submission_url for z in self.file_ext):
                         print(submission_url)
                         file_name = submission_url.rsplit('/', 1)[1]
